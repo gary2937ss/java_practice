d24c6e52f564af2b8aeaadf17bcaabe65d9e97fd
RockChen <gary2937ss@users.noreply.github.com>
2026-05-22 14:21:56 +0800

    2026-05-22-Copy by Value

--- src/main/java/com/financeblog/Main.java
+++ src/main/java/com/financeblog/Main.java
@@ -1,26 +1,14 @@
 package com.financeblog;
 
-import javax.swing.*;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public Main() {
-
+    public static void changeValue(int a) {
+        a = 20;
     }
-
-    public int caculate(int a, int b) {
-        return a / b;
-    }
-
+    // Copy by Value
     public static void main(String[] args) {
-
-        Main main = new Main();
-        System.out.println(main.caculate(15, 3));
-        // Java Swing 裡面用來跳出「輸入視窗」 的方法
-        JOptionPane.showInputDialog("");
-
-
+        int a = 10;
+        changeValue(a);
+        System.out.println(a);
     }
 }
