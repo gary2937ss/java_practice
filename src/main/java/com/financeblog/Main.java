885ba53607c8a929ca63def91ffe0c9ecb1122c5
RockChen <gary2937ss@users.noreply.github.com>
2026-05-22 16:08:24 +0800

    2026-05-22-Recursion and Stack Overflow 1. 堆疊 stack的概念

--- src/main/java/com/financeblog/Main.java
+++ src/main/java/com/financeblog/Main.java
@@ -1,27 +1,27 @@
 package com.financeblog;
 
 public class Main {
-
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
-        // change a and b value.
-        // 1. 先設定一個暫存變數
-        int temp = 0;
-        // 2. 將 a 先指向 temp 此時 temp 是 5
-        temp = a;
-        // 3. 將著將 b 指向給 a 即完成 a值轉 b值 的交換
-        a = b;
-        // 4.目前確定 a 是 6 了，再將 temp=6值給b
-        b = temp;
-
-        int[] array = {1,2,3,4,5,6,7,8};
-        int tempArray = array[0];
-        array[0] = array[3];
-        array[3] = tempArray;
-
-        for (int n : array) {
-            System.out.print(n + " ");
+//     ---------------造成 StackOverflowError 的狀況-------
+//    public static void printHello() {
+//        printHello();
+//    }
+//
+//    public static void main(String[] args) {
+//        printHello();
+//
+//    }
+//    --------------------------------------------------
+    public static void printHello(int i) {
+        // 當 i 為 0 停止該遞迴
+        if (i == 0) {
+            return;
         }
+        // 每一次遞迴減少一次
+        System.out.println("開始堆疊倒數!" + i);
+        printHello(i - 1);
+        System.out.println("開始拿出stack預備!" + i);
+    }
+    public static void main(String[] args) {
+        printHello(10);
     }
 }
